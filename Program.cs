11b1b3f7663697615674937320fe135891c186fb
Robert Witt <robert.witt@sap.com>
2020-02-01 13:54:16 +0100

    Project setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ddd_tutorial_atm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Domain-Driven Design Tutorial
+
+This is a .NET sample app following Domain-Driven Design principles based on the course [Domain-Driven Design in Practice]() on PluralSight.
